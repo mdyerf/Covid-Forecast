67e71ed766a968d1aa5351bc688cbdd9ddf951db
mdyerf <mdyerf1380@gmail.com>
2021-11-19 16:03:31 +0330

    statistics styles and API

--- src/api/stats.js
+++ src/api/stats.js
@@ -1,11 +1,20 @@
 import axios from "axios";
 
-const BASE_URL = "https://api.covidactnow.org/v2/counties.json?county=IR&apiKey=08dbf7a304a64ca890f066ff8382e542";
+const BASE_URL = "https://corona-api.com/countries";
 
 export async function getStats(country, time) {
   try {
+    const res = await axios.get(`${BASE_URL}/${country}`);
+    return res.data.data;
+  } catch (err) {
+    return console.log(err);
+  }
+}
+
+export async function getCountries() {
+  try {
     const res = await axios.get(`${BASE_URL}`);
-    return res.data;
+    return res.data.data.map(cnt => ({value: cnt.code, label: cnt.name}));
   } catch (err) {
     return console.log(err);
   }
--- src/screens/Statistics.js
+++ src/screens/Statistics.js
@@ -1,14 +1,29 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import { BsFillArrowRightCircleFill } from "react-icons/bs";
+import { GiDeathSkull, GiHealthIncrease } from "react-icons/gi";
+import { IoIosPeople } from "react-icons/io";
+import { MdCoronavirus } from "react-icons/md";
 import { Link } from "react-router-dom";
-import { getStats } from "../api/stats";
+import Select from "react-select";
+import { getCountries, getStats } from "../api/stats";
 import routes from "../constants/routes";
 
 function Statistics(props) {
+  const [countries, setCountries] = useState([]);
+  const [country, setCountry] = useState();
+  const [countryLabel, setCountryLabel] = useState({
+    value: "IR",
+    label: "Iran",
+  });
 
   useEffect(() => {
-    getStats("", "").then((res) => console.log(res));
-  });
+    getCountries().then((res) => setCountries(res));
+  }, []);
+
+  useEffect(() => {
+    getStats(countryLabel.value, "").then((res) => setCountry(res));
+  }, [countryLabel]);
+
   return (
     <>
       <div className="page-title">Recent Covid-19 Statistics</div>
@@ -21,6 +36,104 @@ function Statistics(props) {
           />
         </div>
       </Link>
+      <Select
+        className="select"
+        value={countryLabel}
+        onChange={(sel) => setCountryLabel(sel)}
+        options={countries}
+      />
+      <div className="centeral">
+        <img
+          className="flag-img"
+          src={`https://flagcdn.com/120x90/${countryLabel.value.toLowerCase()}.png`}
+          alt={countryLabel.label}
+        />
+        <h2 className="name-text">{countryLabel.label}</h2>
+        {country && (
+          <div className="stats-table">
+            <div>
+              <div className="stats-row">
+                <div className="icon-container">
+                  <IoIosPeople className="icon people" />
+                  <p>
+                    <b>Population:</b> {country.population}
+                  </p>
+                </div>
+                <div className="icon-container">
+                  <MdCoronavirus className="icon virus" />
+                  <p>
+                    <b>Cases Per Million people: </b>
+                    {
+                      country.latest_data.calculated
+                        .cases_per_million_population
+                    }
+                  </p>
+                </div>
+                <div className="icon-container">
+                  <p>
+                    <GiDeathSkull className="icon death" />
+                    <b>Death Rate: </b>
+                    {country.latest_data.calculated.death_rate}
+                  </p>
+                </div>
+              </div>
+              <div className="stats-row">
+                <div className="icon-container">
+                  <GiHealthIncrease className="icon health" />
+                  <p>
+                    <b>Recovery Rate: </b>
+                    {country.latest_data.calculated.recovery_rate}
+                  </p>
+                </div>
+                <div className="icon-container">
+                  <MdCoronavirus className="icon virus" />
+                  <p>
+                    <b>Confirmed:</b> {country.latest_data.confirmed}
+                  </p>
+                </div>
+                <div className="icon-container">
+                  <GiHealthIncrease className="icon health" />
+                  <p>
+                    <b>Recovered:</b> {country.latest_data.recovered}
+                  </p>
+                </div>
+              </div>
+            </div>
+            <div>
+              <div className="stats-row">
+                <div className="icon-container">
+                  <GiDeathSkull className="icon death" />
+                  <p>
+                    <b>Deaths:</b> {country.latest_data.deaths}
+                  </p>
+                </div>
+                <div className="icon-container">
+                  <MdCoronavirus className="icon virus" />
+                  <p>
+                    <b>Criticals:</b> {country.latest_data.critical}
+                  </p>
+                </div>
+              </div>
+              <div className="stats-row">
+                <div className="icon-container">
+                  <GiDeathSkull className="icon death" />
+                  <p>
+                    <b>Today Deaths:</b> {country.today.deaths}
+                  </p>
+                </div>
+                <div className="icon-container">
+                  <MdCoronavirus className="icon virus" />
+                  <p>
+                    <b>Today Cases:</b> {country.today.confirmed}
+                  </p>
+                </div>
+                <div className="icon-container"></div>
+                <div className="icon-container"></div>
+              </div>
+            </div>
+          </div>
+        )}
+      </div>
     </>
   );
 }
